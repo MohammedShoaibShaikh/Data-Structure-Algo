d76779a583fe2f5b08cbce434ebd52ed86ec262f
Mohammed Shoaib Shak <75257111+MohammedShoaibShaikh@users.noreply.github.com>
2022-08-01 18:14:42 +0530

    Add files via upload

---
+++ Selection-Sort/Selection_Sort.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void Selection(int arr[], int n){
+    for(int i = 0; i < n-1; i++){
+        for(int j = i+1; j < n; j++){
+            if(arr[i] > arr[j]){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+}
+
+int main(){
+    int n; cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    Selection(arr, n);
+    return 0;
+}
